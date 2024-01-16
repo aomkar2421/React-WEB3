afdbc76cd56ddda6ecff4f73371900fdafa7074f
aomkar2421 <omkarjagtap1651@gmail.com>
2024-01-16 23:20:43 +0530

    16-janury

--- practise/src/App.jsx
+++ practise/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Fields from './Fields';
 import './App.css'
 
 function App() {
@@ -6,25 +7,33 @@ function App() {
   const [age, setAge] = useState();
   const [data, setData] = useState([]);
 
-  function addDetails(){
-    setData([...data, {name , age}]);
+  function addDetails() {
+    setData([...data, { name, age }]);
     setName('');
     setAge('');
   }
-  
+
   return (
     <>
       <div className="abc">
-      <input value={name} onChange={ (e) => setName(e.target.value)} type="text" name="" id="" placeholder='enter name'/>
-      <input value={age} onChange={ (e) => setAge(e.target.value) } type="text" name="" id="" placeholder='enter age'/>
-      <button onClick={addDetails} >Add</button>
+        <input value={name} onChange={(e) => setName(e.target.value)} type="text" name="" id="" placeholder='enter name' />
+        <input value={age} onChange={(e) => setAge(e.target.value)} type="text" name="" id="" placeholder='enter age' />
+        <button onClick={addDetails} >Add</button>
       </div>
       <div className="def">
-      <h4>Name</h4>
-      <h4>Age</h4>
-      <h4>Remove</h4>
-      </div>
+        <h4>Name</h4>
+        <h4>Age</h4>
+        <h4>Remove</h4>
       
+        
+        <div className="aaaa">
+          <div className="cdata">
+            {data.map((element, index) => {
+              return <Fields key ={index} name = {element.name} age = {element.age}  />
+            })}
+          </div>
+        </div>
+      </div>
     </>
 
   )
---
+++ practise/src/Fields.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+const Fields = ({name , age}) => {
+  return (
+    <div>
+        <h6>{name}</h6>
+        <h6>{age}</h6>
+    </div>
+  )
+}
+
+export default Fields
